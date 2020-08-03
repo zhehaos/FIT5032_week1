b0b2cd13545d6fe61e7d1beb4033ab28b61bf76c
Zhehao Shen <zshe0029@student.monash.edu>
2020-08-03 16:51:15 +1000

    Add project files.

---
+++ FIT5032_week1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_week1.MvcApplication" Language="C#" %>
---
+++ FIT5032_week1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_week1.Startup))]
+namespace FIT5032_week1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
